267f1039808fa9de85021aae430b3ad087da52cf
Bernardo Sertório <besertorio@gmail.com>
2021-01-09 20:09:53 -0300

    Liskov Substitution Principle

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import ShareButtonTwitter from "./ShareButtonTwitter";
 import ShareButtonFacebook from "./ShareButtonFacebook";
 import ShareButtonLinkedIn from "./ShareButtonLinkedIn";
+import ShareButtonPrint from "./ShareButtonPrint";
 
 const twitter = new ShareButtonTwitter(".btn-twitter", "https://www.youtube.com/rodrigobranas");
 twitter.bind();
 const facebook = new ShareButtonFacebook(".btn-facebook", "https://www.youtube.com/rodrigobranas");
 facebook.bind();
 const linkedin = new ShareButtonLinkedIn(".btn-linkedin", "https://www.youtube.com/rodrigobranas");
+linkedin.bind();
+const print = new ShareButtonPrint(".btn-print", "https://www.youtube.com/rodrigobranas");
 linkedin.bind();
